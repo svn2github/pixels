fdffed68e24b1496996ae63edd2388c7fc27ae41
jerryhuxtable <jerryhuxtable@7f5ab353-688e-ef8e-9cfd-fcda36bce185>
2007-05-26 10:46:08 +0000

    Made BlockFilter do proper averaging of blocks
    git-svn-id: https://svn.java.net/svn/pixels~svn/trunk@16 7f5ab353-688e-ef8e-9cfd-fcda36bce185

--- src/main/java/com/jhlabs/image/BlockFilter.java
+++ src/main/java/com/jhlabs/image/BlockFilter.java
@@ -22,9 +22,23 @@ import java.awt.image.*;
 /**
  * A Filter to pixellate images.
  */
-public class BlockFilter extends TransformFilter {
+public class BlockFilter extends AbstractBufferedImageOp {
 	
 	private int blockSize = 2;
+
+    /**
+     * Construct a BlockFilter.
+     */
+	public BlockFilter() {
+	}
+
+    /**
+     * Construct a BlockFilter.
+	 * @param blockSize the number of pixels along each block edge
+     */
+	public BlockFilter( int blockSize ) {
+		this.blockSize = blockSize;
+	}
 
 	/**
 	 * Set the pixel block size.
@@ -46,17 +60,51 @@ public class BlockFilter extends TransformFilter {
 		return blockSize;
 	}
 
+    public BufferedImage filter( BufferedImage src, BufferedImage dst ) {
+        int width = src.getWidth();
+        int height = src.getHeight();
+		int type = src.getType();
+		WritableRaster srcRaster = src.getRaster();
 
-	public BlockFilter() {
-	}
+        if ( dst == null )
+            dst = createCompatibleDestImage( src, null );
 
-	protected void transformInverse(int x, int y, float[] out) {
-		out[0] = (x / blockSize) * blockSize;
-		out[1] = (y / blockSize) * blockSize;
-	}
+		int[] pixels = new int[blockSize * blockSize];
+        for ( int y = 0; y < height; y += blockSize ) {
+            for ( int x = 0; x < width; x += blockSize ) {
+                int w = Math.min( blockSize, width-x );
+                int h = Math.min( blockSize, height-y );
+                int t = w*h;
+                getRGB( src, x, y, w, h, pixels );
+                int r = 0, g = 0, b = 0;
+                int argb;
+                int i = 0;
+                for ( int by = 0; by < h; by++ ) {
+                    for ( int bx = 0; bx < w; bx++ ) {
+                        argb = pixels[i];
+                        r += (argb >> 16) & 0xff;
+                        g += (argb >> 8) & 0xff;
+                        b += argb & 0xff;
+                        i++;
+                    }
+                }
+                argb = ((r/t) << 16) | ((g/t) << 8) | (b/t);
+                i = 0;
+                for ( int by = 0; by < h; by++ ) {
+                    for ( int bx = 0; bx < w; bx++ ) {
+                        pixels[i] = (pixels[i] & 0xff000000) | argb;
+                        i++;
+                    }
+                }
+                setRGB( dst, x, y, w, h, pixels );
+            }
+        }
+
+        return dst;
+    }
 
 	public String toString() {
-		return "Stylize/Mosaic...";
+		return "Pixellate/Mosaic...";
 	}
 }
 
