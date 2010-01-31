d49d369fe3d5746cfe15c50eaafa167da8ad1412
lbalazscs <lbalazscs@7f5ab353-688e-ef8e-9cfd-fcda36bce185>
2010-01-31 20:55:52 +0000

    commented out unused code
    git-svn-id: https://svn.java.net/svn/pixels~svn/trunk@31 7f5ab353-688e-ef8e-9cfd-fcda36bce185

--- src/main/java/com/jhlabs/image/LensBlurFilter.java
+++ src/main/java/com/jhlabs/image/LensBlurFilter.java
@@ -115,8 +115,8 @@ public class LensBlurFilter extends AbstractBufferedImageOp {
         int tileWidth = 128;
         int tileHeight = tileWidth;
 
-        int adjustedWidth = (int)(width + iradius*2);
-        int adjustedHeight = (int)(height + iradius*2);
+//        int adjustedWidth = (int)(width + iradius*2);
+//        int adjustedHeight = (int)(height + iradius*2);
 
 		tileWidth = iradius < 32 ? Math.min(128, width+2*iradius) : Math.min(256, width+2*iradius);
 		tileHeight = iradius < 32 ? Math.min(128, height+2*iradius) : Math.min(256, height+2*iradius);
